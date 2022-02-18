d367da0622c55c8171608acf972d771b49f1c037
SimonaTalaba <talabasimona2@gmail.com>
2022-02-18 12:38:08 +0200

    cvf

---
+++ src/main/java/com/example/depeyb/DepeYbApplication.java
@@ -0,0 +1,20 @@
+package com.example.depeyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DepeYbApplication {
+
+    public static void main(String[] args) {
+      ConfigurableApplicationContext context= SpringApplication.run(DepeYbApplication.class, args);
+        Alien a= context.getBean(Alien.class);
+        a.show();
+//        Alien b=context.getBean(Alien.class);
+//        b.show();
+        }
+
+
+
+}
---
+++ src/test/java/com/example/depeyb/DepeYbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.depeyb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepeYbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
